f1ba80710269afabb4ea8919624996617976b08d
Jenny V Medina <jenny.medina@sagebase.org>
2025-06-02 12:01:17 -0400

    [hotfix] fix user-submitted files with spaces (#55)
    * fix user-submitted files with spaces
    
    * update d2m test profile gt id
    
    * new gt id
    
    * new function to handle other special characters

--- bin/download_submission.py
+++ bin/download_submission.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
+import os
+import re
 import argparse
-import synapseclient
 
+import synapseclient
 from synapseclient.core.constants import concrete_types
 
 def get_args():
@@ -12,6 +13,31 @@ def get_args():
     parser.add_argument("--file-type", "-f", type=str, required=True, help="The type of file the submission should be")
 
     return parser.parse_args()
+
+def clean_file_name(file_path: str) -> None:
+    """Cleans up the file name by replacing special characters with an underscore.
+    
+    Argument(s):
+        file_path: The file path
+    
+    Returns:
+        None
+    """
+
+    # Use os.path to split the directory and file name
+    dir_name = os.path.dirname(file_path)
+    base_name = os.path.basename(file_path)
+    
+    # Replace any character that is not alphanumeric, dash, or underscore with an underscore
+    base_name_clean = re.sub(r'[^A-Za-z0-9._-]', '_', base_name)
+    
+    # Rename the file if needed
+    if base_name != base_name_clean:
+        file_path_clean = os.path.join(dir_name, base_name_clean)
+        os.rename(file_path, file_path_clean)
+        print(f"Special characters found in file name. File path changed: {file_path} --> {file_path_clean}")
+    else:
+        print(f"File path: {file_path}")
 
 if __name__ == "__main__":
     args = get_args()
@@ -24,7 +50,9 @@ if __name__ == "__main__":
     submission = syn.getSubmission(submission_id, downloadLocation=".")
     entity_type = submission["entity"].concreteType
     file_path = submission["filePath"]
-    
+
+    clean_file_name(file_path)
+
     # TODO: Eventually we want to abstract this logic into the `make_invalid_file` function
     # in model-to-data's `run_docker.py`, and move that out somewhere else.
     invalid_file = f"INVALID_predictions.{file_type}"
@@ -34,7 +62,7 @@ if __name__ == "__main__":
         error_msg = (
             f"Only Files should be submitted. Submission {submission_id} type is: {entity_type}"
         )
-    elif not submission["filePath"].lower().endswith(file_type):
+    elif not file_path.lower().endswith(file_type):
         error_msg = f"Incorrect file type. File type should be {file_type.upper()}"
 
     if error_msg:
